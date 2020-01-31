1effe48dd406a3c1aae72fdc491bd0c78366decd
song <wjddbs000@naver.com>
2020-01-31 13:47:31 +0900

    2장완료

--- EscapeTest/main.c
+++ EscapeTest/main.c
@@ -11,8 +11,20 @@ int main() {
 	printf("%lf\n", 3.141592);
 	printf("%.1lf\n", 3.141592);
 	printf("%.10lf\n", 3.14);
-	printf("%.1lf+%.1lf = %.1lf\n", 10.2, 3.4, 10.2 + 3.4);
+	printf("%.1lf+%.1lf = %.1lf\n\n", 10.2, 3.4, 10.2 + 3.4);
 	
+
+	printf("%.1lf\n", 1e10);
+	printf("%.7lf\n", 3.14e-5);
+	printf("%le\n", 0.0000314);
+	printf("%.2le\n\n", 0.0000314);
+
+	printf("%c\n", 'A');
+	printf("%s\n", "A");
+	printf("%c은  %s입니다.\n", '1', "first");
+	printf("%d\n", 'A');
+	printf("%c\n", 65);
+
 	system("pause");
 	return 0;
 
