55ef28b4ca47a56a84c78fbf816d1424b4175f42
song <wjddbs000@naver.com>
2020-02-03 10:38:22 +0900

    4장 완료

---
+++ OpTestApp/main.c
@@ -0,0 +1,43 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수
+  --------------------------------------------------------------------------------
+  first created - 2020.02.01
+  writer - Hugo MG Sung.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 메인함수
+int main(void) 
+{
+    /*int a = 10, b = 20;
+    int res = 2;
+
+    a += 20;
+    res *= b + 10;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("res = %d\n",res);
+    ------------------------------------
+    int a = 10, b = 20;
+    int res = 0;
+
+    res = (a > b) ? a : b;
+
+    printf("a, res = %d, %d\n", a, res);
+    -------------------------------------
+    int a = 10, b = 12;
+    int res = 0;
+
+    printf("a & b = %d\n", a & b);
+    ---------------------------------------*/
+
+
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
